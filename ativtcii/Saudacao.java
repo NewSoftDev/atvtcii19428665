7d3f195b65ace91a2ac90676ac3ab9cdf4cb36cb
Alan Santana <alansantana.intsi@gmail.com>
2024-06-25 21:27:18 -0300

    Mudança de saudação noite e madruga

--- ativtcii/Saudacao.java
+++ ativtcii/Saudacao.java
@@ -12,12 +12,14 @@ public class Saudacao {
             int hora = Integer.parseInt(partes[0]);
             int minuto = Integer.parseInt(partes[1]);
             
-            if ((hora >= 6 && hora < 13) || (hora == 12 && minuto == 59)) {
+            if ((hora >= 5 && hora < 13) || (hora == 12 && minuto == 59)) {
                 System.out.println("Bom dia Mundo");
             } else if ((hora >= 13 && hora < 19) || (hora == 18 && minuto == 59)) {
                 System.out.println("Boa tarde Mundo");
-            } else {
-                System.out.println("Hora fora do intervalo especificado.");
+            } else if ((hora >= 19 && hora < 24) || (hora == 23 && minuto == 59)) {
+                System.out.println("Boa noite Mundo");
+            } else if ((hora >= 0 && hora < 5) || (hora == 4 && minuto == 59)) {
+                System.out.println("Boa 'madruga' Mundo");
             }
         } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
             System.out.println("Formato de hora inválido.");
